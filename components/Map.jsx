00345e5d168b5f98593c7f18675457386236c4ee
Abhishek Singh <abhinav210702@gmail.com>
2024-01-10 21:29:59 +0530

    added Pins on the map

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,9 +1,8 @@
 "use client";
 import React, { useState } from 'react'
-import ReactMapGL, { Marker } from 'react-map-gl'
+import ReactMapGL, { Marker, Popup } from 'react-map-gl'
 import { getCenter } from 'geolib';
 const Map = ({AllHotels}) => {
-  
 
   const coordinates = AllHotels.map(hotel => ({
     longitude: hotel.long,
@@ -26,17 +25,14 @@ const Map = ({AllHotels}) => {
       {...viewPort}
       onMove={evt => setViewPort(evt.viewPort)}
     >
-      <Marker
-        longitude={center.longitude}
-        latitude={center.latitude}
-        offsetLeft={-20}
-        offsetTop={-10}
-      >
-        
-        <p className='text-2xl cursor-pointer animate-bounce'>📍</p>
-
-      </Marker>
-
+     { AllHotels.map(hotel => (
+        <div key={hotel.long}>
+          <Marker longitude={hotel.long} latitude={hotel.lat} offsetLeft={-20} offsetTop={-10}>
+            <p className='cursor-pointer text-2xl animate-bounce' aria-label='push-pin'>📌</p>
+          </Marker>
+        </div>
+      ))
+}
     </ReactMapGL>
   )
 }
